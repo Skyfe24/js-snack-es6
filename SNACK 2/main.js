a2d81c71d9bc8bd07841b8d244dd23f19988d374
Skyfe24 <fabio.erase@gmail.com>
2023-06-13 16:49:35 +0200

    Snack 2 completato

--- SNACK 2/main.js
+++ SNACK 2/main.js
@@ -42,16 +42,26 @@ const students = [
   
   console.log(students);
   //1. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
-  //2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
-
-
+ 
   const secchioni = students.filter(students =>{
     return students.grades > 70; 
  })
-
+ //2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
  const secchioniPLUS = students.filter(students =>{
-    return students.grades > 70 && secchioni.id > 120; 
+    return students.grades > 70 && students.id > 120; 
  })
+
+ //3.  dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo ES (Marco della Rovere => MARCO DELLA ROVERE);
+ 
+ const studentsUP = students.map(student => student.name.toUpperCase());
 
  console.log(secchioni);
  console.log(secchioniPLUS);
+ console.log(studentsUP);
+  
+
+
+ //const studentsUP = students.map((name) => {
+//    return (name).toUpperCase;
+    
+ // });
