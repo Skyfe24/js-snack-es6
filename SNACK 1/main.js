e6d875aa57759fe1acc0a6abb22c52f45e87e7ce
Skyfe24 <fabio.erase@gmail.com>
2023-06-13 16:09:28 +0200

    Snack 1 completed

--- SNACK 1/main.js
+++ SNACK 1/main.js
@@ -1,1 +1,27 @@
-console.log('js ok');
+console.log("js ok");
+const tableName = "VIP Table";
+//array di invitati
+const guestList = [
+  "Brad Pitt",
+  "Johnny Depp",
+  "Lady Gaga",
+  "Cristiano Ronaldo",
+  "Georgina Rodriguez",
+  "Chiara Ferragni",
+  "Fedez",
+  "George Clooney",
+  "Amal Clooney",
+  "Maneskin",
+];
+
+console.log(guestList);
+
+const placeCards = guestList.map((guest, index) => {
+  return {
+    tableName: tableName,
+    guestName: guest,
+    sediaOccupata: index +1,
+  };
+});
+
+console.log(placeCards);
