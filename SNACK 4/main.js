f9b3360b352da882028b649330a483502d183e37
Skyfe24 <fabio.erase@gmail.com>
2023-06-14 16:06:21 +0200

    Snack 4 array is ready

--- SNACK 4/main.js
+++ SNACK 4/main.js
@@ -1,1 +1,25 @@
-console.log('js ok');
+console.log("js ok");
+// Creare un array di oggetti di squadre di calcio.
+// Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+// nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+// Generare numeri random al posto degli 0 nelle proprietà punti fatti e falli subiti.
+// Infine, usando il destructuring, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const squadre = [
+  {
+    nome: "Bluteam",
+    puntiFatti: "",
+    falliSubiti: "",
+  },
+  {
+    nome: "Redteam",
+    puntiFatti: "",
+    falliSubiti: "",
+  },
+  {
+    nome: "Greenteam",
+    puntiFatti: "",
+    falliSubiti: "",
+  },
+];
+console.log(squadre);
