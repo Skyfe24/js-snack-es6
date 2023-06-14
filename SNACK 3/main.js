98069cae203bd1a96e2a07d3fef3b22f0fbc1ebe
Skyfe24 <fabio.erase@gmail.com>
2023-06-14 15:16:09 +0200

    Snack 3 completed

--- SNACK 3/main.js
+++ SNACK 3/main.js
@@ -1,1 +1,27 @@
-console.log('js ok');
+console.log("js ok");
+//Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: name e weight.
+// Stampare in console la bici con peso minore utilizzando il destructuring
+
+const biciclette = [
+  {
+    name: "Blubici",
+    weight: 7,
+  },
+  {
+    name: "Biancobici",
+    weight: 6,
+  },
+  {
+    name: "Verdebici",
+    weight: 5,
+  },
+];
+
+const { name, weight } = biciclette;
+console.log(biciclette);
+
+const lighter = biciclette.reduce((acc, wei) =>
+  acc.weight < wei.weight ? acc : wei
+);
+console.log(lighter);
